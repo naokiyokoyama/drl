dcd135192856fe1d230dfae2272c633de9968ee2
marcodelgado <marcodelgadoaccess@gmail.com>
2022-02-18 17:58:11 -0500

    Created new repo for new robot. Modified eval.py to run on robot's environment and checkpoint loading runs on cpu. Created real_eval.py is from an older eval script that has now been removed.

--- drl/real_eval.py
+++ drl/real_eval.py
@@ -1,7 +1,15 @@
-# from run import GymWrappedEnv
-from drl.a2c_ppo_acktr.model import Policy
-from locomotion.envs import locomotion_gym_env, locomotion_gym_config
+from locomotion.drl.a2c_ppo_acktr import model
+
+from absl import app
+from absl import logging
+import numpy as np
+import pybullet as p  # pytype: disable=import-error
+from tqdm import tqdm
+
+from locomotion.envs import locomotion_gym_config
 from locomotion.robots import aliengo
+from locomotion.robots import aliengo_robot
+from locomotion.envs import locomotion_gym_env
 
 import argparse
 import torch
@@ -19,10 +27,11 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    checkpoint = torch.load(args.checkpoint, map_location='cpu')
+    checkpoint = torch.load(args.checkpoint, map_location=torch.device("cpu"))
 
     """ Generate config """
     config = checkpoint["config"]
+    config.merge_from_list(args.opts)
     config.defrost()
 
     """ Create environment """
@@ -35,12 +44,11 @@ if __name__ == "__main__":
         on_rack=True,
     )
 
-
     """ Create actor-critic """
-    num_reward_terms = checkpoint["state_dict"][
-        "actor_critic.base.critic_linear.bias"
-    ].shape[0] - 1
-    actor_critic = Policy(
+    num_reward_terms = (
+        checkpoint["state_dict"]["actor_critic.base.critic_linear.bias"].shape[0] - 1
+    )
+    actor_critic = model.Policy(
         env.observation_space.shape,
         env.action_space,
         base_kwargs={
@@ -48,7 +56,6 @@ if __name__ == "__main__":
             "reward_terms": num_reward_terms,
             "hidden_size": config.RL.PPO.hidden_size,
             "mlp_hidden_sizes": config.RL.PPO.mlp_hidden_sizes,
-            "init_weights": False,
         },
     )
     print("\nActor-critic architecture:")
@@ -59,7 +66,7 @@ if __name__ == "__main__":
     """ Load weights """
     actor_critic.load_state_dict(
         {
-            k[len("actor_critic."):]: v
+            k[len("actor_critic.") :]: v
             for k, v in checkpoint["state_dict"].items()
             if k.startswith("actor_critic")
         }
@@ -79,19 +86,14 @@ if __name__ == "__main__":
         step_count = 0
         while not_done[0]:
             step_count += 1
-            (
-                _,
-                action,
-                _,
-                recurrent_hidden_states,
-            ) = actor_critic.act(
+            (_, action, _, recurrent_hidden_states,) = actor_critic.act(
                 torch.FloatTensor(observations).unsqueeze(0).to(device),
                 recurrent_hidden_states,
                 not_done,
-                deterministic=True
+                deterministic=True,
             )
 
             observations, _, done, _ = env.step(action[0].detach().cpu().numpy())
             not_done[0] = not done
 
-        print(f'Episode #{idx + 1} finished in {step_count} steps')
+        print(f"Episode #{idx + 1} finished in {step_count} steps")
