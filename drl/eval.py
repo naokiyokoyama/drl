61cdb57239d0aaea64e252226ef6a4653466c175
marcodelgado <marcodelgadoaccess@gmail.com>
2022-02-22 16:15:53 -0500

    Added another argument in command line that. The arguments should be 'walk' or 'stand'. This argument will now load the proper environment for either walking or standing using their respective policy.

--- drl/eval.py
+++ drl/eval.py
@@ -1,7 +1,7 @@
 # from run import GymWrappedEnv
 from drl.a2c_ppo_acktr.model import Policy
-from locomotion.envs import locomotion_gym_env, locomotion_gym_config
-from locomotion.robots import aliengo
+from locomotion.envs import locomotion_gym_env, walking_locomotion, locomotion_gym_config
+from locomotion.robots import aliengo, aliengo_robot
 
 import argparse
 import torch
@@ -11,6 +11,7 @@ sim_params = locomotion_gym_config.SimulationParameters()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("checkpoint", type=str, help="Path to .pth file")
+    parser.add_argument("environment", type=str, help="Which task we want do: 'stand' or 'walk'")
     parser.add_argument(
         "opts",
         default=None,
@@ -19,27 +20,37 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    checkpoint = torch.load(args.checkpoint, map_location='cpu')
+    checkpoint = torch.load(args.checkpoint, map_location="cpu")
+    environment = args.environment
 
     """ Generate config """
     config = checkpoint["config"]
     config.defrost()
 
     """ Create environment """
-    env = locomotion_gym_env.LocomotionGymEnv(
-        gym_config=locomotion_gym_config.LocomotionGymConfig(
-            simulation_parameters=sim_params
-        ),
-        robot_class=aliengo.Aliengo,
-        is_render=True,
-        on_rack=False,
-    )
-
+    if environment == "stand":
+        env = locomotion_gym_env.LocomotionGymEnv(
+            gym_config=locomotion_gym_config.LocomotionGymConfig(
+                simulation_parameters=sim_params
+            ),
+            robot_class=aliengo.Aliengo,
+            is_render=True,
+            on_rack=False,
+        )
+    elif environment == "walk":
+        env = walking_locomotion.LocomotionWalk(
+            gym_config=locomotion_gym_config.LocomotionGymConfig(
+                simulation_parameters=sim_params
+            ),
+            robot_class=aliengo.Aliengo,
+            is_render=True,
+            on_rack=False,
+        )
 
     """ Create actor-critic """
-    num_reward_terms = checkpoint["state_dict"][
-        "actor_critic.base.critic_linear.bias"
-    ].shape[0] - 1
+    num_reward_terms = (
+        checkpoint["state_dict"]["actor_critic.base.critic_linear.bias"].shape[0] - 1
+    )
     actor_critic = Policy(
         env.observation_space.shape,
         env.action_space,
@@ -59,7 +70,7 @@ if __name__ == "__main__":
     """ Load weights """
     actor_critic.load_state_dict(
         {
-            k[len("actor_critic."):]: v
+            k[len("actor_critic.") :]: v
             for k, v in checkpoint["state_dict"].items()
             if k.startswith("actor_critic")
         }
@@ -79,19 +90,14 @@ if __name__ == "__main__":
         step_count = 0
         while not_done[0]:
             step_count += 1
-            (
-                _,
-                action,
-                _,
-                recurrent_hidden_states,
-            ) = actor_critic.act(
+            (_, action, _, recurrent_hidden_states,) = actor_critic.act(
                 torch.FloatTensor(observations).unsqueeze(0).to(device),
                 recurrent_hidden_states,
                 not_done,
-                deterministic=True
+                deterministic=True,
             )
 
             observations, _, done, _ = env.step(action[0].detach().cpu().numpy())
             not_done[0] = not done
 
-        print(f'Episode #{idx + 1} finished in {step_count} steps')
+        print(f"Episode #{idx + 1} finished in {step_count} steps")
