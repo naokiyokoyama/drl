570ba287a4a2d46151bfe64ceb4f72f25e7b45df
Naoki Yokoyama <naokiyokoyama@github.com>
2022-06-06 22:35:33 -0400

    saving csv file inside tensorboard directory

--- drl/utils/writer.py
+++ drl/utils/writer.py
@@ -1,3 +1,4 @@
+import os
 import os.path as osp
 import shutil
 
@@ -10,9 +11,29 @@ class Writer(SummaryWriter):
             shutil.rmtree(path)
         super().__init__(path, *args, **kwargs)
 
+        # Create a .csv file, removing it if it already exists
+        self.csv_path = osp.join(path, osp.basename(path) + ".csv")
+        if osp.isfile(self.csv_path):
+            os.remove(self.csv_path)
+        self.csv_header = []
+
     def add_multi_scalars(self, data, idx):
+        new_data = [idx]
+        record_header = len(self.csv_header) == 0
         for k, v in data.items():
             self.add_scalar(k, v, idx)
+            new_data.append(v)
+            if record_header:
+                self.csv_header.append(k)
+        new_data = self.list_to_csv_line(new_data)
+        if record_header:
+            new_data = self.list_to_csv_line([idx] + self.csv_header) + new_data
+        with open(self.csv_path, "a") as f:
+            f.write(new_data)
+
+    @staticmethod
+    def list_to_csv_line(in_list):
+        return ",".join([str(i) for i in in_list]) + "\n"
 
     @classmethod
     def from_config(cls, config):
