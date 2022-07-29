a145e27bccb773d831d95ed99a9d264f17611785
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-07-29 19:06:33 -0400

    added debug_random layer initialization for debugging

--- drl/utils/init_layers.py
+++ drl/utils/init_layers.py
@@ -18,3 +18,12 @@ def orthogonal_all_weights(module: nn.Module):
         if hasattr(m, "weight"):
             torch.nn.init.orthogonal_(m.weight, gain=np.sqrt(2))
 
+
+@drl_registry.register_init_layer
+def debug_random(module: nn.Module):
+    for m in module.modules():
+        if hasattr(m, "bias"):
+            torch.nn.init.zeros_(m.bias)
+        if hasattr(m, "weight"):
+            torch.manual_seed(1)
+            torch.nn.init.uniform_(m.weight)
